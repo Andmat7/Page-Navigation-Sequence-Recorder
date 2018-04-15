a9a925596fc879cb0f87d16d3a94702a57252760
Andrés Corredor <andrescorredor20@gmail.com>
2018-04-15 12:03:42 -0500

    change

--- inPages/DetectElements.js
+++ inPages/DetectElements.js
@@ -1,60 +1,73 @@
-var connect = chrome.extension.connect({name: "storeEvent"});
-$('a').on('click', function(action){
-    var aLink = this;
-    var href=aLink.href;
-    var event={}
-    event.data=href;
-    event.path=cssRoute(aLink);
-    if ( href=="" || href=="#" || href==null ) {
-        event.action="click";
-    }else{
-        event.action="redirect";
+var connect = chrome.extension.connect({ name: "storeEvent" });
+$(document).click(function (clickEvent) {
+    let tag = $(clickEvent.target).prop("tagName").toLowerCase();
+    let event={}
+    if (tag == 'a') {
+        event.data = clickEvent.target.href
+        var href = event.data
+        event.path = cssRoute(clickEvent.target);
+        if (href == "" || href == "#" || href == null) {
+            event.action = "click";
+        } else {
+            event.action = "redirect";
+        }
+        storeEvent(event)
     }
-    storeEvent(event)
-})
-$("input[type=text], input[type=password], textarea, select").on('change', function(action) {
-    var event={}
-    event.path=cssRoute(this);
-    event.data=this.value;
-    event.action="change";
+    if (tag == 'input'&& clickEvent.target.type == 'submit') {
+        event.action = "click";
+        event.path = cssRoute(clickEvent.target);
+        storeEvent(event)
+    }
+    event.text = $(clickEvent.target).text();
+    storeText(event)
+});
+$("input[type=text], input[type=password], textarea, select").on('change', function (action) {
+    var event = {}
+    event.path = cssRoute(this);
+    event.data = this.value;
+    event.action = "change";
     storeEvent(event);
 });
-$(":button").on('click', function(obj) {
-    var event={}
-    event.path=cssRoute(this);
-    event.action="click";
+$(":button").on('click', function (obj) {
+    var event = {}
+    event.path = cssRoute(this);
+    event.action = "click";
     storeEvent(event);
 });
 
-function storeEvent(event){
+function storeEvent(event) {
     console.log(event);
-	connect.postMessage(event);
+    connect.postMessage(event);
 }
-
+function storeText(event) {
+    console.log(event);
+    event.action = 'text';
+    //connect.postMessage(event);
+}
 function cssRoute(element) {
-	var ListRoutes = [];
-    $(element).parents().not('html').each(function() {
+    var ListRoutes = [];
+    $(element).parents().not('html').each(function () {
         var route = this.tagName.toLowerCase();
         if (this.className) {
             route += "." + this.className.replace(/ /g, '.');
         }
-        else if(this.id) {
-        	route += "#"+this.id;
-        	ListRoutes.push(route);
-        	return false;
+        else if (this.id) {
+            route += "#" + this.id;
+            ListRoutes.push(route);
+            return false;
         }
         ListRoutes.push(route);
     });
     return onelineCss(ListRoutes, element);
 }
 function onelineCss(ListRoutes, element) {
-	ListRoutes.reverse();
-    var path = ListRoutes.join(" ")+" "+element.tagName;  
-    if(element.className!="") { 
-    	path = path+"."+element.className.replace(/ /g, ".");
+    ListRoutes.reverse();
+    var path = ListRoutes.join(" ") + " " + element.tagName;
+    if (element.className != "") {
+        path = path + "." + element.className.replace(/ /g, ".");
     }
-    if(element.id!="") {
-    	path = "#"+element.id;
+    if (element.id != "") {
+        path = "#" + element.id;
     }
     return path;
 }
--- inPages/playerEvents.js
+++ inPages/playerEvents.js
@@ -2,7 +2,6 @@ chrome.runtime.sendMessage({type: "onStart"}, function(response) {});
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.type == "action") {
-            
             var action=request.action;
             if (action.action=="redirect") {
                 window.location.href = $(action.path).attr('href');
@@ -14,6 +13,12 @@ chrome.runtime.onMessage.addListener(
                 $input.focus();
                 $input.val(action.data);
                 $input.change();
+                sendResponse({ ready: true });
+                return true;
+            }
+            if (action.action=="click") {
+                var $input=$(action.path);
+                $input.click();
                 sendResponse({ ready: true });
                 return true;
             }
