d0e0eb4949930b10e29d44540be8a9b99ac88018
andres <andrescorredor20@gmail.com>
2021-06-02 09:20:48 -0500

    add readem

---
+++ Readme.md
@@ -0,0 +1,36 @@
+# chrome plugin: Page Navigation Sequence Recorder
+
+a browser plugin that records, in JSON format, the sequence of navigation performed by a user on a web page, uploading the sequence to a portal. The plugin is able to record only the navigation sequence, to be processed later by another application.
+
+## capture the following events:
+
+- URL entered
+- Clicks on menus or buttons, identifying the page element.
+- Entry of text or selection of combos, identifying the data entered and the element,.
+- Clicks on other objects, identifying the object.
+- It must be possible to select a text on the page and remember it.
+- Save the result and send it to a url via post, being able to enter a name to the sequence.
+
+## other functionalities:
+
+- to replay the navigation.
+- to edit the content of the output
+- to put comments on each event.
+- to capture the launch of a popup.
+
+## 
+The result obtained will be in JSON format similar to the following
+
+```json
+{
+"Navigation": {
+"Name": "name entered by user",
+"URL": "www.test.com",
+"events": [
+{ "event": "click", "element": "id-menu1", "input": "null"},
+{ "event": "input-text", "element": "name", "input": "pepito"}, "event": "input-text", "element": "name", "input": "pepito"}
+
+
+] }
+
+```
--- inPages/DetectElements.js
+++ inPages/DetectElements.js
@@ -1,140 +1,131 @@
 window.browser = (function () {
-  return window.msBrowser || window.browser || window.chrome;
-})();
+    return window.msBrowser ||
+      window.browser ||
+      window.chrome;
+  })();
 var connect = browser.runtime.connect({ name: "storeEvent" });
 $(document).click(function (clickEvent) {
-  let tag = $(clickEvent.target).prop("tagName").toLowerCase();
-  let event = {};
-  event.url = document.URL;
-  event.path = cssRoute(clickEvent.target);
-  event.domPath = getDomPath(clickEvent.target);
-  if (tag == "a") {
-  } else {
-    if (tag == "input") {
-      if (clickEvent.target.type == "submit") {
-        event.action = "submit-click";
-        storeEvent(event);
-      } else {
-        event.action = "click";
-      }
-    } else {
-      event.action = "click";
-      storeEvent(event);
-    }
-  }
+    let tag = $(clickEvent.target).prop("tagName").toLowerCase();
+    let event = {};
+    event.url = document.URL;
+    event.path = cssRoute(clickEvent.target);
+    event.domPath = getDomPath(clickEvent.target);
+    if (tag == 'a') {
+    }else{
+        if (tag == 'input') {
+            if (clickEvent.target.type == 'submit') {
+                event.action = "submit-click";
+                storeEvent(event)
+            } else {
+                event.action = "click";
+            }
 
-  event.data = $(clickEvent.target).text();
-  storeText(event);
-});
-document.body.addEventListener(
-  "click",
-  function (ClickEvent) {
+        }else{
+            event.action = "click";
+            storeEvent(event)
+        }
+    }
+
+    event.data = $(clickEvent.target).text();
+    storeText(event)
+ });
+document.body.addEventListener('click', function (ClickEvent) {
     let event = {};
     let tag = ClickEvent.target.nodeName.toLowerCase();
     event.url = document.URL;
     event.path = cssRoute(ClickEvent.target);
     event.domPath = getDomPath(ClickEvent.target);
-    if (tag == "a") {
-      event.data = ClickEvent.target.href;
-      var href = event.data;
-      if (href == "" || href == "#" || href == null) {
-        event.action = "click";
-      } else {
-        if (href.startsWith("javascript")) {
-          event.action = "redirect_javascript";
+    if (tag == 'a') {
+        event.data = ClickEvent.target.href
+        var href = event.data
+        if (href == "" || href == "#" || href == null) {
+            event.action = "click";
         } else {
-          event.action = "redirect";
+            if (href.startsWith("javascript")) {
+                event.action = "redirect_javascript";
+            } else {
+
+                event.action = "redirect";
+            }
         }
-      }
-      storeEvent(event);
+        storeEvent(event)
     }
-  },
-  true
-);
+
+}, true);
 $(document).submit(function (submitEvent) {
-  let event = {};
-  event.action = "submit";
-  event.data = submitEvent.target.action;
-  event.url = submitEvent.target.action;
-  event.path = cssRoute(submitEvent.target);
-  storeEvent(event);
+    let event = {};
+    event.action = "submit";
+    event.data = submitEvent.target.action
+    event.url = submitEvent.target.action
+    event.path = cssRoute(submitEvent.target);
+    storeEvent(event)
 });
-document.body.addEventListener("change", function (changeEvent) {
-  var event = {};
-  event.path = cssRoute(changeEvent.target);
-  event.domPath = getDomPath(changeEvent.target);
-  event.data = changeEvent.target.value;
-  event.action = "change";
-  event.url = document.URL;
-  storeEvent(event);
-});
+document.body.addEventListener('change', function (changeEvent) {
+    var event = {}
+    event.path = cssRoute(changeEvent.target);
+    event.domPath = getDomPath(changeEvent.target);
+    event.data = changeEvent.target.value;
+    event.action = "change";
+    event.url = document.URL;
+    storeEvent(event);
+})
 function storeEvent(event) {
-  connect.postMessage(event);
+    connect.postMessage(event);
 }
 function storeText(event) {
-  event.action = "text";
-  connect.postMessage(event);
+    event.action = 'text';
+    connect.postMessage(event);
 }
 function cssRoute(element) {
-  var ListRoutes = [];
-  $(element)
-    .parents()
-    .not("html")
-    .each(function () {
-      var route = this.tagName.toLowerCase();
-      if (
-        this.className &&
-        (typeof element.className === "string" ||
-          element.className instanceof String)
-      ) {
-        route += "." + this.className.replace(/ /g, ".");
-      } else if (this.id) {
-        route += "#" + this.id;
+    var ListRoutes = [];
+    $(element).parents().not('html').each(function () {
+        var route = this.tagName.toLowerCase();
+        if (this.className && (typeof element.className === 'string' || element.className instanceof String)) {
+            route += "." + this.className.replace(/ /g, '.');
+        }
+        else if (this.id) {
+            route += "#" + this.id;
+            ListRoutes.push(route);
+            return false;
+        }
         ListRoutes.push(route);
-        return false;
-      }
-      ListRoutes.push(route);
     });
-  return onelineCss(ListRoutes, element);
+    return onelineCss(ListRoutes, element);
 }
 function onelineCss(ListRoutes, element) {
-  ListRoutes.reverse();
-  var path = ListRoutes.join(" ") + " " + element.tagName;
-  if (
-    element.className != "" &&
-    (typeof element.className === "string" ||
-      element.className instanceof String)
-  ) {
-    path = path + "." + element.className.replace(/ /g, ".");
-  }
-  if (element.id != "") {
-    path = "#" + element.id;
-  }
-  return path;
+    ListRoutes.reverse();
+    var path = ListRoutes.join(" ") + " " + element.tagName;
+    if (element.className != "" && (typeof element.className === 'string' || element.className instanceof String)) {
+        path = path + "." + element.className.replace(/ /g, ".");
+    }
+    if (element.id != "") {
+        path = "#" + element.id;
+    }
+    return path;
 }
 function getDomPath(element) {
-  var stack = [];
-  while (element.parentNode != null) {
-    var sibCount = 0;
-    var sibIndex = 0;
-    for (var i = 0; i < element.parentNode.childNodes.length; i++) {
-      var sib = element.parentNode.childNodes[i];
-      if (sib.nodeName == element.nodeName) {
-        if (sib === element) {
-          sibIndex = sibCount;
+    var stack = [];
+    while (element.parentNode != null) {
+        var sibCount = 0;
+        var sibIndex = 0;
+        for (var i = 0; i < element.parentNode.childNodes.length; i++) {
+            var sib = element.parentNode.childNodes[i];
+            if (sib.nodeName == element.nodeName) {
+                if (sib === element) {
+                    sibIndex = sibCount;
+                }
+                sibCount++;
+            }
         }
-        sibCount++;
-      }
+        if (element.hasAttribute('id') && element.id != '') {
+            stack.unshift(element.nodeName.toLowerCase() + '#' + element.id);
+        } else if (sibCount > 1) {
+            stack.unshift(element.nodeName.toLowerCase() + ':eq(' + sibIndex + ')');
+        } else {
+            stack.unshift(element.nodeName.toLowerCase());
+        }
+        element = element.parentNode;
     }
-    if (element.hasAttribute("id") && element.id != "") {
-      stack.unshift(element.nodeName.toLowerCase() + "#" + element.id);
-    } else if (sibCount > 1) {
-      stack.unshift(element.nodeName.toLowerCase() + ":eq(" + sibIndex + ")");
-    } else {
-      stack.unshift(element.nodeName.toLowerCase());
-    }
-    element = element.parentNode;
-  }
 
-  return stack.slice(1).join(" > "); // removes the html element
+    return stack.slice(1).join(' > '); // removes the html element
 }
